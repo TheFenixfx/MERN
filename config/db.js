cde95f621aad99ddb23708f25019255567d35b24
Jhon <knifefx@gmail.com>
2021-02-10 11:40:16 -0430

    developer connect

--- config/db.js
+++ config/db.js
@@ -3,21 +3,19 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(db,{
-            useNewUrlParser : true, 
-            useUnifiedTopology: true,
-            usecreateIndexes: true
-        })
-        
-        console.log('MOngoDB Conected ...');
-    
-    }catch(err){
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      usecreateIndexes: true,
+      useFindAndModify: false,
+    });
+
+    console.log("MOngoDB Conected ...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
-
-
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -7,22 +7,17 @@ connectDB();
 
 // Init Middleware
 
-app.use(express.json ({ extender: false})  ); 
+app.use(express.json({ extender: false }));
 
-
-app.get('/', (req,res) => res.send('API running'))
+app.get("/", (req, res) => res.send("API running"));
 
 // Define Routes
 
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/post',require('./routes/api/post'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,() => 
-    console.log( 'Server started on port ${PORT}')
-    );
-
+app.listen(PORT, () => console.log("Server started on port ${PORT}"));
